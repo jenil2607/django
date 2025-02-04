35e637b8e96600f357a8f57b6b8ac4fdc88811ad
unknown <jeniltanti98257@gmail.com>
2025-02-04 16:07:00 +0530

    edit modules

--- shop419/mainapp/views.py
+++ shop419/mainapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product
+
+# to help to load template file
+from django.template import loader
+
+# to help return HTTP response to the user for any given request 
+from django.http import HttpResponse
 
 # Create your views here.
+def homeView(request):
+    products = Product.objects.all()
+    context = {
+        'product_list' : products
+    }
+    template = loader.get_template('home.html')
+    return HttpResponse(template.reader(context, request))
--- shop419/shop419/settings.py
+++ shop419/shop419/settings.py
@@ -121,6 +121,15 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+
+# configration the user-uploaded media storage location
+# in development sesrver, we are going use local folder inside the peroject
+# to store the side media, when deploying, we can replase thr filepath
+# with a url to the media server
+
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
