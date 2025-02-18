f734ef1b84412dabb4097eb1f0c7b6aaf9d1a70f
unknown <jeniltanti98257@gmail.com>
2025-02-18 15:58:09 +0530

    add delete button

--- shop419/mainapp/urls.py
+++ shop419/mainapp/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("", homeView, name="homepage"),
     path("about", aboutView, name="aboutpage"),
     
-    path("products/add", AddProduct.as_view(), name = "addproduct"),                  # C
-    path("Products/int<pk>", ProductDetails.as_view(), name = "prod_details"),        # R
-    path("Products/edit/<int:pk>", UpdateProduct.as_view(), name = "editproduct"),    # u
-    path("Products/del/<int:pk>", DeleteProduct.as_view(), name = "delproduct"),      # D
+    path("products/add",           AddProduct.as_view(),      name = "addproduct"),                  # C
+    path("Products/int<pk>",       ProductDetails.as_view(),  name = "prod_details"),        # R
+    path("Products/edit/<int:pk>", UpdateProduct.as_view(),   name = "editproduct"),    # u
+    path("Products/del/<int:pk>",  DeleteProduct.as_view(),   name = "delproduct"),      # D
 ] 
